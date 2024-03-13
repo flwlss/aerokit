d738071a3b02ba74dc570dc2499c3a2a932ff55a
Dmitry <dimaandreev1337@gmail.com>
2024-03-13 14:41:09 +0700

    new fields in forms

--- src/components/RepairForm/RepairForm.jsx
+++ src/components/RepairForm/RepairForm.jsx
@@ -4,60 +4,71 @@ import styles from "./style.module.scss";
 export default function RepairForm() {
   return (
     <form className={styles.form}>
-          <div className={styles.formField}>
-            <label htmlFor="airCompany">Авиакомпания</label>
-            <input type="text" id="airCompany" placeholder="Введите название" />
-          </div>
-          <div className={styles.formField}>
+      <div className={styles.formField}>
+        <label htmlFor="airCompany">Авиакомпания</label>
+        <input type="text" id="airCompany" placeholder="Введите название" />
+      </div>
+      {/* <div className={styles.formField}>
             <label htmlFor="vsType">Тип ВС</label>
             <input type="text" id="vsType" placeholder="Введите тип ВС" />
           </div>
           <div className={styles.formField}>
             <label htmlFor="regNum">Рег. номер ВС</label>
             <input type="text" id="regNum" placeholder="Введите номер" />
-          </div>
-          <div className={styles.formField}>
-            <label htmlFor="planNums">Чертежные номера</label>
-            <input type="text" id="planNums" placeholder="Введите номера" />
-          </div>
-          <div className={styles.formField}>
+          </div> */}
+      <div className={styles.formField}>
+        <label htmlFor="planNums">Чертежные номера</label>
+        <input type="text" id="planNums" placeholder="Введите номера" />
+      </div>
+      {/* <div className={styles.formField}>
             <label htmlFor="serialNum">Серийные номера</label>
             <input type="text" id="serialNum" placeholder="Введите номера" />
           </div>
           <div className={styles.formField}>
             <label htmlFor="name">Тип ремонтируемого компонента</label>
             <input type="text" id="name" placeholder="Введите тип" />
-          </div>
-          <div className={styles.formField}>
-              <label htmlFor="quantity">Количество</label>
-              <input type="text" id="quantity" placeholder="Введите количество" />
-            </div>
-            <div className={styles.formField}>
-              <label htmlFor="deadLines">Желаемая дата оказания услуги</label>
-              <input type="text" id="deadLines" placeholder="Введите дату" />
-            </div>
-          <div className={styles.formField}>
+          </div> */}
+      <div className={styles.formField}>
+        <label htmlFor="quantity">Количество</label>
+        <input type="text" id="quantity" placeholder="Введите количество" />
+      </div>
+      <div className={styles.formField}>
+        <label htmlFor="deadLines">Желаемая дата оказания услуги</label>
+        <input type="text" id="deadLines" placeholder="Введите дату" />
+      </div>
+      {/* <div className={styles.formField}>
             <label htmlFor="deliveryAddress">Адрес поставки</label>
             <input
               type="text"
               id="deliveryAddress"
               placeholder="Введите адрес"
             />
-          </div>
-          <div className={styles.cells}>
-
-            <div className={styles.formField}>
-              <label htmlFor="email">E-mail</label>
-              <input type="text" id="email" placeholder="@" />
-            </div>
-            <div className={styles.formField}>
-              <label htmlFor="phone">Телефон</label>
-              <input type="text" id="phone" placeholder="+7" />
-            </div>
-          </div>
-          <div className={styles.submitBtnContainer}>
-            <RequestButton onClick={() => {}}>Отправить</RequestButton>
-          </div>
-        </form>
-  )
+          </div> */}
+      <div className={styles.cells}>
+        <div className={styles.formField}>
+          <label htmlFor="email">E-mail</label>
+          <input type="text" id="email" placeholder="@" />
+        </div>
+        <div className={styles.formField}>
+          <label htmlFor="phone">Телефон</label>
+          <input type="text" id="phone" placeholder="+7" />
+        </div>
+      </div>
+      <div className={styles.formField}>
+        <label htmlFor="description">Описание заявки</label>
+        <textarea type="text" id="description" placeholder="Введите описание" />
+      </div>
+      <div className={styles.formField}>
+        <label htmlFor="files">Прикрепить CMM</label>
+        <label className={styles.fileInput} htmlFor="files">
+          <img src="/src/assets/svg/file.svg" alt="" />
+          <p>Прикрепить</p>
+        </label>
+        <input style={{ display: "none" }} id="files" type="file" />
+      </div>
+      <div className={styles.submitBtnContainer}>
+        <RequestButton onClick={() => {}}>Отправить</RequestButton>
+      </div>
+    </form>
+  );
 }
--- src/components/RequestForm/RequestForm.jsx
+++ src/components/RequestForm/RequestForm.jsx
@@ -4,51 +4,59 @@ import styles from "./style.module.scss";
 export default function RequestForm() {
   return (
     <form className={styles.form}>
-          <div className={styles.formField}>
-            <label htmlFor="airCompany">Авиакомпания</label>
-            <input type="text" id="airCompany" placeholder="Введите название" />
-          </div>
-          <div className={styles.formField}>
-            <label htmlFor="vsType">Тип ВС</label>
-            <input type="text" id="vsType" placeholder="Введите тип ВС" />
-          </div>
-          <div className={styles.formField}>
-            <label htmlFor="planNums">Чертежные номера</label>
-            <input type="text" id="planNums" placeholder="Введите номера" />
-          </div>
-          <div className={styles.formField}>
-            <label htmlFor="name">Тип продукции</label>
-            <input type="text" id="name" placeholder="Введите тип" />
-          </div>
-          <div className={styles.formField}>
-              <label htmlFor="quantity">Количество</label>
-              <input type="text" id="quantity" placeholder="Введите количество" />
-            </div>
-            <div className={styles.formField}>
-              <label htmlFor="deadLines">Желаемая дата поставки</label>
-              <input type="text" id="deadLines" placeholder="Введите дату" />
-            </div>
-          <div className={styles.formField}>
-            <label htmlFor="deliveryAddress">Адрес поставки</label>
-            <input
-              type="text"
-              id="deliveryAddress"
-              placeholder="Введите адрес"
-            />
-          </div>
-          <div className={styles.cells}>
-            <div className={styles.formField}>
-              <label htmlFor="email">E-mail</label>
-              <input type="text" id="email" placeholder="@" />
-            </div>
-            <div className={styles.formField}>
-              <label htmlFor="phone">Телефон</label>
-              <input type="text" id="phone" placeholder="+7" />
-            </div>
-          </div>
-          <div className={styles.submitBtnContainer}>
-            <RequestButton onClick={() => {}}>Отправить</RequestButton>
-          </div>
-        </form>
-  )
+      <div className={styles.formField}>
+        <label htmlFor="airCompany">Авиакомпания</label>
+        <input type="text" id="airCompany" placeholder="Введите название" />
+      </div>
+      <div className={styles.formField}>
+        <label htmlFor="vsType">Тип ВС</label>
+        <input type="text" id="vsType" placeholder="Введите тип ВС" />
+      </div>
+      <div className={styles.formField}>
+        <label htmlFor="planNums">Чертежные номера</label>
+        <input type="text" id="planNums" placeholder="Введите номера" />
+      </div>
+      <div className={styles.formField}>
+        <label htmlFor="name">Тип продукции</label>
+        <input type="text" id="name" placeholder="Введите тип" />
+      </div>
+      <div className={styles.formField}>
+        <label htmlFor="quantity">Количество</label>
+        <input type="text" id="quantity" placeholder="Введите количество" />
+      </div>
+      <div className={styles.formField}>
+        <label htmlFor="deadLines">Желаемая дата поставки</label>
+        <input type="text" id="deadLines" placeholder="Введите дату" />
+      </div>
+      <div className={styles.formField}>
+        <label htmlFor="deliveryAddress">Адрес поставки</label>
+        <input type="text" id="deliveryAddress" placeholder="Введите адрес" />
+      </div>
+      <div className={styles.cells}>
+        <div className={styles.formField}>
+          <label htmlFor="email">E-mail</label>
+          <input type="text" id="email" placeholder="@" />
+        </div>
+        <div className={styles.formField}>
+          <label htmlFor="phone">Телефон</label>
+          <input type="text" id="phone" placeholder="+7" />
+        </div>
+      </div>
+      <div className={styles.formField}>
+        <label htmlFor="description">Описание заявки</label>
+        <textarea type="text" id="description" placeholder="Введите описание" />
+      </div>
+      <div className={styles.formField}>
+        <label htmlFor="files">Прикрепить фото</label>
+        <label className={styles.fileInput} htmlFor="files">
+          <img src="/src/assets/svg/file.svg" alt="" />
+          <p>Прикрепить</p>
+        </label>
+        <input style={{ display: "none" }} id="files" type="file" />
+      </div>
+      <div className={styles.submitBtnContainer}>
+        <RequestButton onClick={() => {}}>Отправить</RequestButton>
+      </div>
+    </form>
+  );
 }
